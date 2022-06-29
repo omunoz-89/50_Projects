13abd79b1e8d55db5de8dac0f57fa18883ea4e7c
omunoz <steven.120189@gmail.com>
2022-06-28 18:21:58 -0700

    Made CSS changes, added JS for active panels

--- Expanding_Cards/script.js
+++ Expanding_Cards/script.js
@@ -1,1 +1,14 @@
-const panel = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach((panel) => {
+    panel.addEventListener('click', () => {
+        removeActivePanel()
+        panel.classList.add('active')
+    })
+})
+
+function removeActivePanel() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
